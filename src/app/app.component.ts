a19b80cd40de642d6e3d1e1864389a09b1e904be
ashishchopra24 <49956840+ashishchopra24@users.noreply.github.com>
2020-04-14 17:41:37 +0530

    DataService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountsService} from './accounts.service';
+import {LoggingService} from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService, LoggingService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit{
+
+  accounts: {name: string, status: string}[] =[];
+
+  constructor(private acccountService: AccountsService){}
+
+  ngOnInit(): void {
+    this.accounts = this.acccountService.accounts;
+  }
+
 
   onAccountAdded(newAccount: {name: string, status: string}) {
     this.accounts.push(newAccount);
